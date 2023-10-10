e43a0aa2c159b4d7b853ae5123d875b11361972a
MyGuru <32195301+myprivateguru@users.noreply.github.com>
2023-10-10 08:53:03 +0530

    initial template

---
+++ src/main/java/com/universal/containx/ContainxApplication.java
@@ -0,0 +1,13 @@
+package com.universal.containx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainxApplication.class, args);
+	}
+
+}
